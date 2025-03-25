a366f919d085d8e3631e371acd425a169e2ed355
Ahron Pollak <ahronrocks51@gmail.com>
2025-03-25 11:24:38 -0400

    feat: finish ASM questions.
    The last two questions focused on endianness importance and undefined
    behavior.

--- user/call.c
+++ user/call.c
@@ -3,6 +3,7 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+/*
 int g (int x) {
   return x + 3;
 }
@@ -10,8 +11,14 @@ int g (int x) {
 int f(int x) {
   return g(x);
 }
+*/
 
 void main(void) {
-  printf("%d %d\n", f(8)+1, 13);
+  //printf("%d %d\n", f(8)+1, 13);
+  /*
+  unsigned int i = 0x00646c72;
+	printf("H%x Wo%s", 57616, &i);
+  */
+  printf("x=%d, y=%d", 3);
   exit(0);
 }
